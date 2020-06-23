6f1b01bfd8ecdc65eaccc9a333ddf4293e4d68a4
unknown <sara.reidy.21@gmail.com>
2020-06-22 22:33:57 -0400

    trying a thing

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const server = require('./server');
 
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => {
-    console.log(`listening on port: ${port}`);
-});
+server.listen(port, () => 
+    console.log(`listening on port: ${port}`)
+);
